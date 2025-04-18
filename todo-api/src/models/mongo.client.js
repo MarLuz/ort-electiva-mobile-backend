b2ae85780f93c8812afa62391c3babcede8e0ea0
MarLuz <mluzdesign@gmail.com>
2025-04-18 09:36:31 -0300

    Changing connection string

--- todo-api/src/models/mongo.client.js
+++ todo-api/src/models/mongo.client.js
@@ -4,12 +4,10 @@ const connectMongoDB = async () => {
   const MONGODB_CONNECTION_STRING = process.env.MONGODB_CONNECTION_STRING;
   const MONGODB_DATABASE_NAME = process.env.MONGODB_DATABASE_NAME;
 
-  await mongoose.connect(
-    `${MONGODB_CONNECTION_STRING}/${MONGODB_DATABASE_NAME}`,
-    {
-      serverSelectionTimeoutMS: 3000,
-    }
-  );
+  await mongoose.connect(`${MONGODB_CONNECTION_STRING}`, {
+    serverSelectionTimeoutMS: 3000,
+    dbName: MONGODB_DATABASE_NAME,
+  });
   console.log("Conexión a MongoDB establecida correctamente");
 };
 
